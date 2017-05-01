08b6fdaef40885561e3e26658f89acb98af6fddf
PrimeForce <zaharko.19988@gmail.com>
2017-05-01 21:53:24 +0300

    2

--- Utils.py
+++ Utils.py
@@ -3,8 +3,7 @@ from time import *
 
 
 def get_last_line(filename, encod='ansi'):
-    """Returns last line of file with <filename>
-    0000609a035b03906e8b57939d46d99764af52210909d4990542dc380c4e9812"""
+    """Returns last line of file with <filename>"""
     with open(filename, encoding=encod) as file:
         file.seek(0, os.SEEK_END)
         position = file.tell()
@@ -17,7 +16,6 @@ def get_last_line(filename, encod='ansi'):
             line += next_char
             position -= 1
         return line[::-1]
-
 
 
 def time_sec_ago(delta):
