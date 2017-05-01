40eaa72c9839f2b498ff420864a60b055d665531
PrimeForce <zaharko.19988@gmail.com>
2017-05-01 19:45:19 +0300

    2

--- Utils.py
+++ Utils.py
@@ -2,8 +2,9 @@ import os
 from time import *
 
 
-def get_last_line(filename):
-    with open(filename) as file:
+def get_last_line(filename, encod='ansi'):
+    """Returns last line of file with <filename>"""
+    with open(filename, encoding=encod) as file:
         file.seek(0, os.SEEK_END)
         position = file.tell()
         line = ''
@@ -11,13 +12,15 @@ def get_last_line(filename):
             file.seek(position)
             next_char = file.read(1)
             if next_char == "\n":
-                return reversed(line)
+                return line[::-1]
             line += next_char
             position -= 1
-        return reversed(line)
+        return line[::-1]
 
 
 def time_sec_ago(delta):
+    """Accepts <delta> seconds and returns
+    current time - delta"""
     t = time()-delta
     return strftime('%H:%M:%S', localtime(t))
 
