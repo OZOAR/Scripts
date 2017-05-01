075adeb9a5b9c98c16ea7918258f13ac3aadcd38
Vadim <VadimRezunik@yandex.ru>
2017-05-01 18:47:10 +0300

    newh

--- Runnable.py
+++ Runnable.py
@@ -1,20 +1,22 @@
 import subprocess as sp
+from os import startfile, remove
 import time
+import datetime
+EXE = 'EthDcrMiner64.exe'
+BAT = 'start.bat'
+FILE = 'loggg.noappend.txt'
+KILLER = 'close_ethdcrminer.bat'
+task = sp.Popen(EXE)
+task.kill()
 
-
-def open_exe(path):
-    task = sp.Popen(path)
-    print('Opened {}'.format(path))
-    time.sleep(5)
-    task.kill()
-    print('Task killed')
-    pass
-
-def open_bat(path):
-    object = sp.Popen(path)
-    print('{} Opened'.format(path))
-    time.sleep(5)
-    object.kill()
-
-
-open_bat('cut.bat')
+while True:
+    startfile(BAT)
+    while True:
+        time.sleep(5)
+        with open(FILE) as file:
+            buffer = ''.join((s for s in file))
+        if buffer.find('exit'):
+            startfile(KILLER)
+            remove(FILE)
+            print('Miner is restarting: '.format(datetime.datetime.now()))
+            break
