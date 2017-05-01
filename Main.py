2d25c87e6010dcfca7ac959dcf9878921a519ad8
Vadim <VadimRezunik@yandex.ru>
2017-05-01 14:07:58 +0300

    1

---
+++ Main.py
@@ -0,0 +1,10 @@
+class Main():
+    def __init__(self,greeting):
+        self.greeting = greeting
+
+    def output(self):
+        return self.greeting
+
+instance = Main('Hi')
+print(instance.output())
+print(1)
