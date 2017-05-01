baf421f87fa3054d3c2ac1cd9b14822f9a2bd8be
PrimeForce <zaharko.19988@gmail.com>
2017-05-01 19:50:16 +0300

    2

--- Utils.py
+++ Utils.py
@@ -3,7 +3,8 @@ from time import *
 
 
 def get_last_line(filename, encod='ansi'):
-    """Returns last line of file with <filename>"""
+    """Returns last line of file with <filename>
+    0000609a035b03906e8b57939d46d99764af52210909d4990542dc380c4e9812"""
     with open(filename, encoding=encod) as file:
         file.seek(0, os.SEEK_END)
         position = file.tell()
