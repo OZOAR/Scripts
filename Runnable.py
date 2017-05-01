8fed5a29eadbbdd56b8293f8762757702b639a13
PrimeForce <zaharko.19988@gmail.com>
2017-05-01 16:12:47 +0300

    2

--- Runnable.py
+++ Runnable.py
@@ -1,5 +1,17 @@
+import subprocess as sp
+import time
+
+
 def open_exe(path):
+    task = sp.Popen(path)
+    print('Opened {}'.format(path))
+    time.sleep(5)
+    task.kill()
+    print('Task killed')
     pass
+
 
 def open_bat(path):
     pass
+
+open_exe('oaip.exe')
